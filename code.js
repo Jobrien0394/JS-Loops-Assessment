f6cc7de0e96fc176c6a6bc8759b7a2e19606a1c4
Jobrien0394 <84590243+Jobrien0394@users.noreply.github.com>
2021-07-08 17:37:39 -0400

    Completed Loops Assignment.

--- code.js
+++ code.js
@@ -29,11 +29,45 @@ document.write("<h3>Now analyzing the array: " + numbers + "</h3>");
 document.write("<ul>");
 // Write your code here 👇
 
-let largestNumber;
+let largestNumber = 0;
+let smallestNumber = numbers[0];
+let sum = 0;
+let avgNum = 0;
+let evenNumbers = [];
+let oddNumbers = [];
+let divByEight = [];
 
+for (let index = 0; index < numbers.length; index += 1) {
+  
+  if (numbers[index] > largestNumber) {
+    largestNumber = numbers[index]; }
+
+    if (numbers[index] < smallestNumber) {
+      smallestNumber = numbers[index] }
+
+      sum += numbers[index]
+      
+      avgNum = sum / numbers.length;
+
+      if (numbers[index] % 2 === 0) {
+      evenNumbers.push(numbers[index]);
+      }
+      if (numbers[index] % 2 !== 0) {
+        oddNumbers.push(numbers[index]);
+      }
+      if (numbers[index] % 8 === 0) {
+        divByEight.push(numbers[index]);
+      }
+  }
 
 // Then render each li:
 document.write("<li>Largest Number: " + largestNumber + "</li>");
+document.write("<li>Smallest Number: " + smallestNumber + "</li>");
+document.write("<li>The Sum is: " + sum + "</li>");
+document.write("<li>The Average is: " + avgNum + "</li>");
+document.write("<li>The Even Numbers are: " + evenNumbers + "</li>");
+document.write("<li>The Odd Numbers are: " +oddNumbers + "</li>");
+document.write("<li> Numbers Divisible by 8 are: " +divByEight + "</li>");
 // etc...
 
 
